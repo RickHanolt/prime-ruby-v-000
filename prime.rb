3764abe18fb2ae867f6b782f58a6e6edf1645e23
Richard Hanolt <rhanolt@gmail.com>
2020-05-05 23:37:00 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,5 @@
 def prime?(number)
     prime_check = (2..100).to_a
     prime_check.collect{|integer| number % integer == 0 if integer < number}
-    prime_check.all?(true)
+    prime_check.all?(false)
 end

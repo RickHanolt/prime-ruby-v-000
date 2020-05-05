1c839aa14b1ac644ce86f8cf5198dba8ef95835d
Richard Hanolt <rhanolt@gmail.com>
2020-05-05 23:25:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,1 @@
-def prime?(number)
-  i = 3
-  if number == 2 || number == 3
-    true
-  elsif number.even? && number != 2 || number <= 1
-    false
-  elsif number.odd? && number > 3
-    until number%i == 0
-      i += 2
-    end
-    number == i
-  end
-end
+

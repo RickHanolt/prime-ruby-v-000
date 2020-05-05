aae0cb0f50dbc8865c55348f5da91143062c04d0
Richard Hanolt <rhanolt@gmail.com>
2020-05-05 23:53:03 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,28 @@
+def prime?(number)
+  if number == 1 || number == -1 || number == 0
+    return false
+  end
+
+  if number > 0
+    counter = 2
+    while counter < number
+      if number % counter == 0
+        return false
+      end
+      counter += 1
+    end
+    true
+  elsif number < 0
+    counter = -2
+    while counter > number
+      if number % counter == 0
+        return false
+      end
+      counter -= 1
+    end
+    true
+  end
+end
 
 def prime?(number)
   i = 3
